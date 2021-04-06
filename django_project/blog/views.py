1cb8ef318da134a6783aa82aa0c1c9a1b43fd96e
MARIA PAZ SUBIABRE <mpsubiabre@uc.cl>
2021-04-06 19:45:19 -0400

    bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'pachi',
+        'title': 'soy el titulo',
+        'content': 'holi'
+    },
+    {
+        'author': 'flo',
+        'title': 'soy el titulo2',
+        'content': 'holi2'
+    }
+]
 
 # Create your views here.
+def home(request):
+   # return HttpResponse('<h1>Homee</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'abput'})
